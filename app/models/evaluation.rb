f9db4921442f3360f79cb45258a81c2676154b3c
daglitch <duhglitch@gmail.com>
2010-12-14 01:42:41 -0700

    remove working

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,4 +1,4 @@
 class Evaluation < ActiveRecord::Base
   has_many :topics, :dependent => :destroy
-  accepts_nested_attributes_for :topics
+  accepts_nested_attributes_for :topics, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
 end
