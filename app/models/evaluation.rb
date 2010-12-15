2f440756ee1187eb908cd23824fac98657b21145
daglitch <duhglitch@gmail.com>
2010-12-15 14:05:08 -0700

    Ratings added and working somewhat

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,4 +1,5 @@
 class Evaluation < ActiveRecord::Base
+  #acts_as_rateable
   has_many :topics, :dependent => :destroy
   accepts_nested_attributes_for :topics, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
 end
