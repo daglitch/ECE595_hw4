543acd2729499d2d96714403f4efad29ca06c083
daglitch <duhglitch@gmail.com>
2010-12-15 19:58:07 -0700

    fixed ratings not getting saved.

--- app/models/subtopic.rb
+++ app/models/subtopic.rb
@@ -2,5 +2,5 @@ class Subtopic < ActiveRecord::Base
     belongs_to :topic
     #has_many :ratings
     has_many :ratings, :dependent => :destroy
-    accepts_nested_attributes_for :ratings, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
+    accepts_nested_attributes_for :ratings, :allow_destroy => true
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,6 @@
 class Topic < ActiveRecord::Base
   belongs_to :evaluation
   has_many :subtopics, :dependent => :destroy
+  #has_many :ratings, :dependent => :destroy
   accepts_nested_attributes_for :subtopics, :reject_if => lambda { |a| a[:content].blank?}, :allow_destroy => true
 end
